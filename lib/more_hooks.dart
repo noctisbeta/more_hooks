3655a34140252b90e201ba19706ea30e8cfe6181
Janez Emeršič <janez.emersic@tovarnaidej.si>
2022-11-18 07:57:09 +0100

    commit

---
+++ lib/more_hooks.dart
@@ -0,0 +1,3 @@
+export 'src/countdown_hook.dart';
+export 'src/periodic_hook.dart';
+export 'src/route_aware_hook.dart';
---
+++ lib/src/periodic_hook.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+/// Periodic hook.
+void usePeriodic(Duration period, void Function() callback) {
+  return use(
+    _PeriodicHook(period, callback),
+  );
+}
+
+class _PeriodicHook extends Hook<void> {
+  const _PeriodicHook(
+    this.period,
+    this.callback,
+  );
+
+  final void Function() callback;
+
+  final Duration period;
+
+  @override
+  __PeriodicHookState createState() => __PeriodicHookState();
+}
+
+class __PeriodicHookState extends HookState<void, _PeriodicHook> {
+  late final Timer _timer;
+
+  @override
+  void initHook() {
+    super.initHook();
+
+    _timer = Timer.periodic(
+      hook.period,
+      (timer) {
+        hook.callback.call();
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  void build(BuildContext context) {}
+}
